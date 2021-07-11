5afbac0d48e67e78397c69017e856989f9229bbf
Oleh Yashtulov <oleh_yashtulov@epam.com>
2021-07-11 18:32:03 +0300

    Created Todos controller

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates_presence_of :name, :created_by, :completion_status
+  validates_presence_of :name, :created_by
 end
