ca46d15c223d5dcd01960ef676fcff2ad6926c4e
Oleh Yashtulov <oleh_yashtulov@epam.com>
2021-07-13 18:23:26 +0300

    Fixed old syntax in migration file: add_todos_association_to_project

--- db/migrate/20210713113841_add_todos_association_to_project.rb
+++ db/migrate/20210713113841_add_todos_association_to_project.rb
@@ -1,10 +1,6 @@
 class AddTodosAssociationToProject < ActiveRecord::Migration[6.1]
-  def self.up
+  def change
     add_column :todos, :project_id, :integer
     add_index 'todos', ['project_id'], :name => 'index_project_id'
-  end
-
-  def self.down
-    remove_column :todos, :project_id
   end
 end
